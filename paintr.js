5cf3a666d795aa7fd0fc1a4120c557bfdb12eb53
Matteo Guglielmetti <matteo.guglielmetti@click-shop.it>
2016-02-10 13:51:42 +0100

    Added customization options
    New options include background and pixel (grid) size. Started commenting the code. Improved handling of the colors.

--- paintr.js
+++ paintr.js
@@ -16,27 +16,34 @@ function Color (r,g,b,transparency) {
 
 Color.prototype.returnRGBCode = function () {
   console.log(this.rgb);
-  return this.rgb
+  return this.rgb;
 };
+
+//GENERATION OPTIONS
+var base_seed = 2, //Size of the left half of the grid, used to calculate entire size
+  base_height = (base_seed * 2 ) + 1,    //Final size of the grid
+  //FIXME Brush size leaves empty white lines between the pixels
+  brush_size = 100 / base_height, //Brush size
+  drawBg = true; //Not functional for now, keep true
 
 function randomizeHalfGrid (grid) {
   var map = grid;
-  for (var i = 0; i < 2; i++) {
+  for (var i = 0; i < base_seed; i++) {
     map[i] = [];
-    for (var j = 0; j < 5; j++) {
+    for (var j = 0; j < base_height; j++) {
       map[i][j] = Math.floor(Math.random() * 2);
     }
   }
   return map;
-};
+}
 
 function randomizeCenterGrid (){
   var center = [];
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < base_height; i++) {
     center.push(Math.floor(Math.random() * 2));
   }
   return center;
-};
+}
 
 var red = new Color (200,0,0);
 var green = new Color (0,200,0);
@@ -44,7 +51,7 @@ var blue = new Color (0,0,200);
 
 function randomizeRGB(color) {
   if (typeof(color) === "undefined") {
-    color = Object;
+    color = new Color();
     color.r = Math.floor((Math.random() * 200) + 0);
     color.g = Math.floor((Math.random() * 200) + 0);
     color.b = Math.floor((Math.random() * 200) + 0);
@@ -56,54 +63,67 @@ function randomizeRGB(color) {
 function drawAll () {
   var grid = [];
 
-  var grid = randomizeHalfGrid(grid);
+  grid = randomizeHalfGrid(grid);
   var center = randomizeCenterGrid();
 
   var canvas = document.getElementById('canvas');
   if (canvas.getContext) {
     var drw = canvas.getContext("2d");
     var color = randomizeRGB();
-    console.log(color);
-    drw.fillStyle = "rgb(" + color.r + "," + color.g + "," + color.b + ")";
+    var bg_color = randomizeRGB();
+
+    color.returnRGBCode();
+    bg_color.returnRGBCode();
 
     var coordx = 0, coordy = 0;
 
-    for (var i = 0; i < 2; i++) {
-      for (var j = 0; j < 5; j++) {
+    for (var i = 0; i < base_seed; i++) {
+      for (var j = 0; j < base_height; j++) {
         console.log("Drawing normal " + grid[i][j] + " at " + coordx + "," +coordy);
         if (grid[i][j]) {
-          drw.fillRect (coordx, coordy, 20, 20);
-        };
-        coordy += 20;
-      };
-      coordx += 20;
+          drw.fillStyle = "rgb(" + color.r + "," + color.g + "," + color.b + ")";
+        }
+        else if(drawBg && !grid[i][j]) {
+          drw.fillStyle = "rgb(" + bg_color.r + "," + bg_color.g + "," + bg_color.b + ")";
+        }
+        drw.fillRect (coordx, coordy, brush_size, brush_size);
+        coordy += brush_size;
+      }
+      coordx += brush_size;
       coordy = 0;
-    };
+    }
 
-    for (var i = 0; i < 5; i++) {
-      console.log("Drawing center " + center[i] + " at " + coordx + "," +coordy);
-      if (center[i]) {
-        drw.fillRect (coordx, coordy, 20, 20);
-      };
-      coordy += 20;
-    };
+    for (var x = 0; x < base_height; x++) {
+      console.log("Drawing center " + center[x] + " at " + coordx + "," +coordy);
+      if (center[x]) {
+        drw.fillStyle = "rgb(" + color.r + "," + color.g + "," + color.b + ")";
+      }
+      else if(drawBg) {
+        drw.fillStyle = "rgb(" + bg_color.r + "," + bg_color.g + "," + bg_color.b + ")";
+      }
+      drw.fillRect (coordx, coordy, brush_size, brush_size);
+      coordy += brush_size;
+    }
 
-    coordx += 20;
+    coordx += brush_size;
     coordy = 0;
     //Print the grid in reverse column order
-    var ireverse = 1;
-    for (var k = 0; k < 2 ; k++) {
-      for (var z = 0; z < 5; z++) {
+    var ireverse = base_seed - 1;
+    for (var k = 0; k < base_seed ; k++) {
+      for (var z = 0; z < base_height; z++) {
         console.log("Drawing reverse " + grid[ireverse][z] + " at " + coordx + "," +coordy);
         if (grid[ireverse][z]) {
-          drw.fillRect (coordx, coordy, 20, 20);
-        };
-        coordy += 20;
-      };
+          drw.fillStyle = "rgb(" + color.r + "," + color.g + "," + color.b + ")";
+        }
+        else if(drawBg){
+          drw.fillStyle = "rgb(" + bg_color.r + "," + bg_color.g + "," + bg_color.b + ")";
+        }
+        drw.fillRect (coordx, coordy, brush_size, brush_size);
+        coordy += brush_size;
+      }
       ireverse-= 1;
-      coordx += 20;
+      coordx += brush_size;
       coordy = 0;
-    };
-
-  };
-};
+    }
+  }
+}
